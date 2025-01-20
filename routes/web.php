fdf6b8f30804ea81a961efe06b17c21355baef39
sopharaa <schhay2@paragoniu.edu.kh>
2025-01-20 17:30:30 +0700

    hello

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-URL::forceScheme('https');
+
 Route::get('/', function () {
     return view('voter.index');
 })->name('front.index');
